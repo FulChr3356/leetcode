05f10acd0a517689f64a1c20261ac594cdb71cda
Christopher Fulton <51063994+FulChr3356@users.noreply.github.com>
2022-07-10 17:01:20 -0400

    added majority

--- src/io/fulchr3356/Main.java
+++ src/io/fulchr3356/Main.java
@@ -4,6 +4,21 @@ import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
+
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i: nums){
+            if(map.containsKey(i)){
+                map.put(i,map.get(i) + 1);
+                if(map.get(i) > nums.length / 2)
+                    return i;
+            }
+            else
+                map.put(i,1);
+
+        }
+        return nums[0];
+    }
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         Map<ListNode,ListNode> mapA = new LinkedHashMap<>();
         Map<ListNode,ListNode> mapB = new LinkedHashMap<>();
