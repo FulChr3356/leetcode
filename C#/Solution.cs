0b66088d0981360ae8d0efdd652b03c116d6b198
Christopher Fulton <51063994+FulChr3356@users.noreply.github.com>
2022-07-13 21:44:14 -0400

    added c# and remove value

---
+++ C#/Solution.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int size = nums.Length;
+        bool replace = false;
+        Queue queue = new Queue();
+        for(int i = 0; i < nums.Length; i++){
+            if(nums[i] == val){
+                queue.Enqueue(i);
+                replace = true;
+                size--;
+                continue;
+            }
+            if(replace == true){
+                nums[(int)queue.Dequeue()] = nums[i];
+                queue.Enqueue(i);
+            }
+        }
+        return size;
+    }
+}
