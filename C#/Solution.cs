a5ab9d5a85c2b092b29e72f25a6bd4c7aa7ef212
Christopher Fulton <51063994+FulChr3356@users.noreply.github.com>
2022-07-14 22:23:48 -0400

    added unfinished 3sum

--- C#/Solution.cs
+++ C#/Solution.cs
@@ -1,10 +1,25 @@
 public class Solution {
+    public IList<IList<int>> ThreeSum(int[] nums) {
+        Dictionary<int[],Integer> map = new Dictionary<int[],Integer>();
+        Dictionary<Integer,Integer> map2 = new Dictionary<Integer,Integer>(); 
+        List 
+        for(int i = 0;l i < nums.Length -1; i++)
+             for(int j = i + 1; j < nums.Length; j++)
+                map.add(new int[] {i,j},nums[i] + nums[j]);
+        IList<IList<int>> list = new IIList<IList<int>>();
+        int[] k;
+        foreach(int[] duos in list.Keys){
+            k = nums.Select((b,i) => b + duos[0] + duos[1] == 0 ? i : -1).Where(i => i != -1).ToArray();
+        }
+        
+    }
+
 
     public int StrStr(string haystack, string needle) {
         return haystack.IndexOf(needle);
     }
 
-    
+
     public int RemoveElement(int[] nums, int val) {
         int size = nums.Length;
         bool replace = false;
