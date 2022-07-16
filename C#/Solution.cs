fd794d1c688224c218bb1b15a4e9c94c2e45b329
Christopher Fulton <51063994+FulChr3356@users.noreply.github.com>
2022-07-15 21:07:19 -0400

    updated threesum
    still incomplete

--- C#/Solution.cs
+++ C#/Solution.cs
@@ -1,17 +1,21 @@
 public class Solution {
     public IList<IList<int>> ThreeSum(int[] nums) {
-        Dictionary<int[],Integer> map = new Dictionary<int[],Integer>();
-        Dictionary<Integer,Integer> map2 = new Dictionary<Integer,Integer>(); 
-        List 
-        for(int i = 0;l i < nums.Length -1; i++)
-             for(int j = i + 1; j < nums.Length; j++)
+        Dictionary<int[],int> map = new Dictionary<int[],int>();
+        for(int i = 0; i < nums.Length -1; i++){
+             for(int j = i + 1; j < nums.Length; j++){
                 map.add(new int[] {i,j},nums[i] + nums[j]);
+             }
+        }
         IList<IList<int>> list = new IIList<IList<int>>();
         int[] k;
         foreach(int[] duos in list.Keys){
             k = nums.Select((b,i) => b + duos[0] + duos[1] == 0 ? i : -1).Where(i => i != -1).ToArray();
+            foreach (int i in k){
+                if(i != duos[0] && i != duos[1])
+                    list.add(new int [] {duos[0],duos[1], i} );
+            }
         }
-        
+        return list;
     }
 
 
